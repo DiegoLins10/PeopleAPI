6f84efd8f0b8e845fa897fe1839c0d76159a23e4
diegolins10 <dede140126>
2022-02-12 19:43:53 -0300

    propriedades alterações

--- People.API/Startup.cs
+++ People.API/Startup.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace People.API
 {
@@ -26,6 +27,7 @@ namespace People.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
