1d2a323c7dce3603b5dab0b06a5046b49a616bb3
diegolins10 <dede140126>
2022-02-12 19:43:20 -0300

    Extensões

---
+++ People.API/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace People.API.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+    }
+}
